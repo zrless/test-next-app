59a608d63a1197e4138f1bfb87874c077ab61372
张锐 <zhangrui01@shouqianba.com>
2021-03-19 09:38:45 +0800

    feat: 测试路由

--- pages/about/[id].js
+++ pages/about/[id].js
@@ -1,13 +1,18 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
+import { useRouter } from "next/router";
+import Link from "next/link";
 export default function About() {
   const router = useRouter();
   const { id, a } = router.query;
 
-  return <div>
-    <h1>About/{id}?a={a}</h1>
-    <Link href="/">主页</Link>
-    <div></div>
-    <Link href="/my">我的</Link>
-  </div>;
+  return (
+    <div>
+      <h1>
+        About/{id}
+        {a ? "?a=" + a : ""}
+      </h1>
+      <Link href="/">主页</Link>
+      <div></div>
+      <Link href="/my">我的</Link>
+    </div>
+  );
 }
--- pages/my/index.js
+++ pages/my/index.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 export default function My() {
+  const router = useRouter();
 
   return <div>
     <h1>My</h1>
-    <Link href="/about/123">关于我们</Link>
+    <div onClick={() => router.push("/about/789")}>关于我们</div>
     <div></div>
     <Link href="/">主页</Link>
   </div>
